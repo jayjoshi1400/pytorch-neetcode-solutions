f7d095137e3980ca23c40435b1b450f3608c44ba
jmjoshi <jmjoshi@ncsu.edu>
2024-07-25 17:56:56 -0400

    Solved gradient descent

---
+++ gradient_descent.py
@@ -0,0 +1,9 @@
+class Solution:
+    def get_minimizer(self, iterations: int, learning_rate: float, init: int) -> float:
+        cur = init
+        
+        while range(iterations):
+            grad = 2*cur
+            cur = cur - (learning_rate * grad)
+            iterations-=1
+        return round(cur,5)
